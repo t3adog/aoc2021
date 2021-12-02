b136100e26b74360c1454ab0bcbc58a62d1f04a8
Petr Belyakov <pabelyak@mts.ru>
2021-12-02 20:35:48 +0300

    little bit refactoring

--- src/test/kotlin/day01/Day01KtTest.kt
+++ src/test/kotlin/day01/Day01KtTest.kt
@@ -8,14 +8,14 @@ internal class Day01KtTest {
 
     @Test
     fun testPart1() {
-        val input = readInput("day1", "Day01_test")
+        val input = readInput("day01", "Day01_test")
         val result = part1(input)
         assertEquals(7, result)
     }
 
     @Test
     fun testPart2() {
-        val input = readInput("day1", "Day01_test")
+        val input = readInput("day01", "Day01_test")
         val result = part2(input)
         assertEquals(5, result)
     }
