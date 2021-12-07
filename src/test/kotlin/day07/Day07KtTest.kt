071ebae509ca07942244dd0c0e55ddaf8c9a87f6
Petr Belyakov <pabelyak@mts.ru>
2021-12-07 20:32:45 +0300

    day07 added

---
+++ src/test/kotlin/day07/Day07KtTest.kt
@@ -0,0 +1,23 @@
+package day07
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import readInput
+
+internal class Day07KtTest {
+
+    @Test
+    fun part1() {
+        val input = readInput("day07", "Day07_test")
+        val result = part1(input)
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun part2() {
+        val input = readInput("day07", "Day07_test")
+        val result = part2(input)
+        assertEquals(0, result)
+    }
+}
