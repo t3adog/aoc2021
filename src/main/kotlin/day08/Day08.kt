2cde40b09d77bcc7abb02b320f6db6215063a457
Petr Belyakov <pabelyak@mts.ru>
2021-12-08 10:36:36 +0300

    ⌚️ day08 - template has been added

---
+++ src/main/kotlin/day08/Day08.kt
@@ -0,0 +1,17 @@
+package day08
+
+import readInputToIntList
+
+fun part1(input: List<Int>): Int {
+    return 0
+}
+
+fun part2(input: List<Int>): Int {
+    return 0
+}
+
+fun main() {
+    val input = readInputToIntList("day08", "Day08")
+    println(part1(input))
+    println(part2(input))
+}
---
+++ src/test/kotlin/day08/Day08KtTest.kt
@@ -0,0 +1,21 @@
+package day08
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import readInputToIntList
+
+internal class Day08KtTest {
+
+    @Test
+    fun part1() {
+        val input = readInputToIntList("day08", "Day08_test")
+        assertEquals(0, part1(input))
+    }
+
+    @Test
+    fun part2() {
+        val input = readInputToIntList("day08", "Day08_test")
+        assertEquals(0, part2(input))
+    }
+}
