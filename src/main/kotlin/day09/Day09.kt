cf118f340781ea6d7586d4877294db01c1f60fa3
Petr Belyakov <pabelyak@mts.ru>
2021-12-09 09:17:34 +0300

    day09 - part01 is completed

--- src/main/kotlin/day09/Day09.kt
+++ src/main/kotlin/day09/Day09.kt
@@ -2,12 +2,38 @@ package day09
 
 import readMatrix
 
+fun isLeast(y: Int, x: Int, matrix: List<List<Int>>): Boolean {
+    var neighbours = mutableListOf<Int>()
+    if (y - 1 > -1) {
+        neighbours.add(matrix[y - 1][x])
+    }
+    if (y + 1 < matrix.size) {
+        neighbours.add(matrix[y + 1][x])
+    }
+    if (x - 1 > -1) {
+        neighbours.add(matrix[y][x - 1])
+    }
+    if (x + 1 < matrix[y].size) {
+        neighbours.add(matrix[y][x + 1])
+    }
+    return matrix[y][x] < (neighbours.minOrNull() ?: 0)
+}
+
 fun part1(input: List<List<Int>>): Int {
-   return 0
+    val minimalValues = mutableListOf<Int>()
+
+    for (y: Int in 0..input.size - 1) {
+        for (x: Int in 0..input[y].size - 1) {
+            if (isLeast(y, x, input)) {
+                minimalValues.add(input[y][x])
+            }
+        }
+    }
+    return minimalValues.map { it + 1 }.sum()
 }
 
 fun part2(input: List<List<Int>>): Int {
-   return 0
+    return 0
 }
 
 fun main() {
