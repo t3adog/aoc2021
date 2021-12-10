cdef70b34c65b5d35ffa104bc905caaeb1208d5d
Petr Belyakov <pabelyak@mts.ru>
2021-12-10 11:56:21 +0300

    day10 - part01 is complete

--- src/main/kotlin/day10/Day10.kt
+++ src/main/kotlin/day10/Day10.kt
@@ -2,8 +2,38 @@ package day10
 
 import readInput
 
+val symbol_map = mapOf<String, String>(
+    "(" to ")",
+    "{" to "}",
+    "[" to "]",
+    "<" to ">"
+)
+
+val score_table = mapOf(
+    ")" to 3,
+    "]" to 57,
+    "}" to 1197,
+    ">" to 25137
+)
+
 fun part1(input: List<String>): Int {
-    return 0
+    var score = 0
+    for (line in input) {
+        val openedChars = mutableListOf<String>()
+        for (char in line.toCharArray().map { it.toString() }) {
+            if (char in symbol_map.keys) {
+                openedChars.add(char)
+            } else {
+                if (symbol_map.get(openedChars.last()) == char) {
+                    openedChars.removeLast()
+                } else {
+                    score += score_table[char] ?: 0
+                    break
+                }
+            }
+        }
+    }
+    return score
 }
 
 fun part2(input: List<String>): Int {
