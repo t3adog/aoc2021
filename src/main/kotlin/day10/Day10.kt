61f2bcccb8ee283502a378f9b41d592b8855e44a
Petr Belyakov <pabelyak@mts.ru>
2021-12-10 10:18:23 +0300

    day10 - day10 template has been created

---
+++ src/main/kotlin/day10/Day10.kt
@@ -0,0 +1,17 @@
+package day10
+
+import readInput
+
+fun part1(input: List<String>): Int {
+    return 0
+}
+
+fun part2(input: List<String>): Int {
+    return 0
+}
+
+fun main() {
+    val input = readInput("day10", "Day10")
+    println(part1(input))
+    println(part2(input))
+}
