4d0f81dd3d21ca3ad838eeba46aef2e1233701ca
Petr Belyakov <pabelyak@mts.ru>
2021-12-14 19:07:09 +0300

    day14 - added day 14

---
+++ src/main/kotlin/day14/Day14.kt
@@ -0,0 +1,16 @@
+package day14
+
+import readInput
+
+fun part1(input: List<String>): Int {
+    return 0
+}
+
+fun part2(input: List<String>): Int {
+    return 0
+}
+
+fun main() {
+    println(part1(readInput("Day14", "Day14")))
+    println(part2(readInput("Day14", "Day14")))
+}
---
+++ src/test/kotlin/day14/Day14KtTest.kt
@@ -0,0 +1,19 @@
+package day14
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import readInput
+
+internal class Day14KtTest {
+
+    @Test
+    fun part1() {
+        assertEquals(1, part1(readInput("Day14", "Day14_test")))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(1, part2(readInput("Day14", "Day14_test")))
+    }
+}
