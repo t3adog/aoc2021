4da741072cdcd87c6de099cf166a07a8e837f416
Petr Belyakov <pabelyak@mts.ru>
2021-12-15 07:50:14 +0300

    day15 has been added

---
+++ src/test/kotlin/day15/Day15KtTest.kt
@@ -0,0 +1,19 @@
+package day15
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import readInput
+
+internal class Day15KtTest {
+
+    @Test
+    fun part1() {
+        assertEquals(1, part1(readInput("Day15", "Day15_test")))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(1, part2(readInput("Day15", "Day15_test")))
+    }
+}
